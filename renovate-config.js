b215d0233d88b588c5102af175dd489bec8d005e
splincode <omaxphp@yandex.ru>
2023-08-16 15:37:57 +0300

    chore: debug

---
+++ renovate-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  $schema: "https://docs.renovatebot.com/renovate-schema.json",
+  extends: ["github>taiga-family/renovate-config"],
+  repositories: ["taiga-family/tui-editor"],
+  skipInstalls: false,
+  onboarding: false,
+  allowCustomCrateRegistries: true,
+  logFileLevel: "debug",
+  logFile: "renovate.log",
+  trustLevel: "high",
+  commitMessageSuffix: "[renovate self hosted]",
+};
